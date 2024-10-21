8a754722e7165d1c407a9deeb3311671feb6835e
Lionel <lpei1900@gmail.com>
2024-10-20 22:57:50 -0400

    Set up the database

---
+++ db/pools.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+});
+
+// Export the pool for use in other files
+module.exports = pool;
