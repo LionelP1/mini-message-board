fed75e180b1cee0ad463273f8d3388f836969de2
Lionel <lpei1900@gmail.com>
2024-11-18 23:06:11 -0500

    Made changes to the pools

--- db/pools.js
+++ db/pools.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+
+  // Uncomment the following if you need to test with individual parameters later
+  // host: process.env.DB_HOST,                // Database host
+  // user: process.env.DB_USER,                // Database username
+  // password: process.env.DB_PASSWORD,        // Database password
+  // database: process.env.DB_NAME,            // Database name
+  // port: process.env.DB_PORT || 5432,        // Database port
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
